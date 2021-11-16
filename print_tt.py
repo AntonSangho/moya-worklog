311833ad4f2bfd2660be3e89a8ee201c128d834c
AntonSangho <sanghoemail@gmail.com>
2021-11-16 06:45:13 +0000

    티티섬용 용지와 실행파일추가

---
+++ print_tt.py
@@ -0,0 +1,46 @@
+"""
+Requirement
+gcc
+cups-devel(libcups-2dev)
+python3-devel (python3 dev)
+
+Printe on demand with rasberrypi (https://www.hackster.io/glowascii/print-on-demand-with-raspi-d74619)
+GPIO pin:
+
+"""
+# !/bin/python
+
+import RPi.GPIO as GPIO
+import time
+import os
+import cups
+import random 
+
+conn = cups.Connection()
+printers = conn.getPrinters()
+file1 = "/home/pi/moya-worklog/image/w1_tt.png"
+file2 = "/home/pi/moya-worklog/image/w2_tt.png"
+file3 = "/home/pi/moya-worklog/image/w3_tt.png"
+file4 = "/home/pi/moya-worklog/image/w4_tt.png"
+file5 = "/home/pi/moya-worklog/image/w5_tt.png"
+filelist = [file1, file2, file3, file4, file5]
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.setwarnings(False)
+GPIO.setup(20, GPIO.OUT)
+print("LED on")
+GPIO.output(20, GPIO.HIGH)
+time.sleep(1)
+
+#os.system('lp /usr/share/cups/data/testprint')
+
+def Printtest(channel):
+    print('Printing...')
+    conn.printFile('BIXOLON_SRP-330II', random.choice(filelist), "working diary", {})
+
+GPIO.add_event_detect(21, GPIO.RISING, callback=Printtest, bouncetime=2000)
+
+while 1:
+    time.sleep(1)
